1c3ac60371d987ed6eb72639d5dc1c51eee8cfc1
Alexis Dietl <alexisdietl@gmail.com>
2021-04-16 14:00:49 -0300

    gitignore

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-const http = require('http')
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.end('Welcome to our page!')
-    }
-    if(req.url === '/about'){
-        res.end('Here is our short history')
-    }
-    res.end(`<h1>OOPS!</h1>
-        <p>We cant seem to find the page</p>
-        <a href="/">back home</a>`)
-})
-
-server.listen(5000)
+const _ = require('lodash');
+const items = [1,[2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems);
