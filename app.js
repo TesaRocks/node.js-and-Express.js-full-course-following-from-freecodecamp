d94760224c4f39676f7c573d47446ada20740bda
Alexis Dietl <alexisdietl@gmail.com>
2021-04-16 12:19:21 -0300

    async read-write

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const {readFileSync, writeFileSync} = require('fs')
+const {readFile, writeFile} = require('fs')
 
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
-
-writeFileSync('./content/result-sync.txt', `Here is the result: ${first}, ${second}`, {flag: 'a'})
+readFile('./content/first.txt','utf8', (err, result)=>{
+    if(err){
+        console.log(err);
+        return
+    }
+    const first = result
+    readFile('./content/second.txt', 'utf8', (err, result)=>{
+        if(err){
+            console.log(err);
+            return
+        }
+        const second = result
+        writeFile('./content/result-async.txt',`Here is the result tess: ${first}, ${second}`
+        ,(err, result)=>{
+            if(err){
+                console.log(err);
+                return
+            }
+            console.log(result);
+        })
+    })
+})
