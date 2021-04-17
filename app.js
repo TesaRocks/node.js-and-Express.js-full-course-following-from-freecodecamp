36e55586bdd1aebcb696272424b55a2a26bd53a9
Alexis Dietl <alexisdietl@gmail.com>
2021-04-17 16:53:36 -0300

    practicing with async tasks

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const http = require('http')
-const server = http.createServer((req, res)=>{
+// const http = require('http')
+// const server = http.createServer((req, res)=>{
+
+// })
+
+// server.listen(80, ()=>{
+//     console.log('Server is listening on port 5000');
+// })
+
+const {readFile} = require('fs')
+console.log('start');
+readFile('./content/first.txt','utf8', (err, result)=>{
+    if(err){
+        console.log(err);
+        console.log("japi");
+        return
+    }
+    console.log(result)
+    console.log('done wit tito');
 
 })
-
-server.listen(80, ()=>{
-    console.log('Server is listening on port 5000');
-})
+console.log('starting next task');
