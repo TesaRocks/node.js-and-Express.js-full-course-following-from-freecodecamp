fe4e6e67a4196da00138c4be73dd447cb79bbba3
Alexis Dietl <alexisdietl@gmail.com>
2021-04-17 18:00:38 -0300

    promises

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 // const http = require('http')
-// const server = http.createServer((req, res)=>{
-
+// const server = http.createServer((req,res)=>{
+// if(req.url === '/'){
+//     res.end('home page pihs')
+// } else if(req.url === '/about'){
+//     res.end('about page')
+// }
+// else res.end('error page')
 // })
-
-// server.listen(80, ()=>{
-//     console.log('Server is listening on port 5000');
+// server.listen(3000, ()=>{
+//     console.log('server pijon');
 // })
 
 const {readFile} = require('fs')
-console.log('start');
-readFile('./content/first.txt','utf8', (err, result)=>{
-    if(err){
-        console.log(err);
-        console.log("japi");
-        return
-    }
-    console.log(result)
-    console.log('done wit tito');
+const getText = (path)=>{
+    return new Promise((resolve, reject)=>{
+        readFile(path, 'utf8', (err, data)=>{
+            if(err){
+                reject(err)
+            } else{
+                resolve(data)
+            }
+        })
 
-})
-console.log('starting next task');
+    })
+}
+// getText('./content/first.txt').then(result=>console.log(result)).catch(err =>console.log(err))
+
+const start = async()=>{
+    const first = await getText('./content/first.txt')
+    console.log(first);
+}
+
+start()
