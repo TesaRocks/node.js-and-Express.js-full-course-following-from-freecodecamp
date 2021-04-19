b52cf41a22774a292a81ca977bc43669d035c2a1
Alexis Dietl <alexisdietl@gmail.com>
2021-04-19 16:04:58 -0300

    async await

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-// const http = require('http')
-// const server = http.createServer((req,res)=>{
-// if(req.url === '/'){
-//     res.end('home page pihs')
-// } else if(req.url === '/about'){
-//     res.end('about page')
-// }
-// else res.end('error page')
-// })
-// server.listen(3000, ()=>{
-//     console.log('server pijon');
-// })
 
-const {readFile} = require('fs')
-const getText = (path)=>{
-    return new Promise((resolve, reject)=>{
-        readFile(path, 'utf8', (err, data)=>{
-            if(err){
-                reject(err)
-            } else{
-                resolve(data)
-            }
-        })
-
-    })
-}
-// getText('./content/first.txt').then(result=>console.log(result)).catch(err =>console.log(err))
+const {readFile, writeFile} = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
 const start = async()=>{
-    const first = await getText('./content/first.txt')
-    console.log(first);
+    try{
+        const first = await readFile('./content/first.txt', 'utf8')
+        const sec = await readFile('./content/second.txt', 'utf8')
+        await writeFile('./content/result-mind-grenade', `This is asweome: ${first} ${sec}`, {flag:'a'})
+        console.log(first, sec);
+    }catch(error){
+        console.log(error.message);
+    }
 }
 
 start()
+
+
+
+
+// const getText = (path)=>{
+//     return new Promise((resolve, reject)=>{
+//         readFile(path, 'utf8', (err, data)=>{
+//             if(err){
+//                 reject(err)
+//             } else{
+//                 resolve(data)
+//             }
+//         })
+
+
+//     })
+// }
+// getText('./content/first.txt').then(result=>console.log(result)).catch(err =>console.log(err))
